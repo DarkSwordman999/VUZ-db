c26c4cc8c7f6ab1bfe76ed5b99c9f8103e6ad0bb
DarkSwordman999 <DarkSwordman999@users.noreply.github.com>
2026-06-06 12:07:58 +0300

    feat: добавить конфигурацию БД в db_config.py

---
+++ db_config.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Конфигурация для базы данных ВУЗа
+DB_CONFIG = {
+    'dbname': 'vuz_lab5',
+    'user': 'uliavladimirovna',
+    'password': '',
+    'host': 'localhost'
+}
